b8f2bd638217f0743111a1aafef9831a1e337c8c
Igor Zaharov <lars.zaharov@gmail.com>
2018-09-26 00:23:05 +0300

    Change predictor-corrector method

--- lab_03/main.py
+++ lab_03/main.py
@@ -6,12 +6,9 @@
     Adams prognosis-correction method of 2, 3, 4-th orders
 """
 
-import numpy as np
+import tableprint as tp
 import matplotlib.pyplot as plt
 from multiprocessing import Process
-from scipy import interpolate
-from scipy import integrate
-from runge_kutta_interpolation import runge_kutta_interpolation
 
 
 def _display_plot(x_nodes_list, y_nodes_list):
@@ -20,7 +17,8 @@ def _display_plot(x_nodes_list, y_nodes_list):
     """
     for x_nodes, y_nodes, order in zip(x_nodes_list, y_nodes_list, range(2, 5)):
         plt.plot(x_nodes, y_nodes, label='{0}'.format(order), linewidth=0.3)
-    plt.title("Adams prognosis-correction method")
+    plt.plot(x_nodes_list[-1], [explicit_solution(x) for x in x_nodes_list[-1]], linewidth=0.4)
+    plt.title('Adams prognosis-correction method')
 
     plt.ioff()              # disable window interactive mode
     plt.legend(loc='best')  # enable labels in plot
@@ -34,30 +32,26 @@ def display_plot_async(x_nodes_list, y_nodes_list):
     process.start()
 
 
-def adam_prognosis_correction_method(func, x_0, y_0, segment_end, step, accuracy=4):
+def predictor_corrector_method(func, x_0, y_0, segment_end, step, accuracy):
     """
-
-    :param func:
-    :param x_0:
-    :param y_0:
-    :param segment_end:
-    :param step:
-    :param accuracy:
-    :return:
+        Explicit Euler predictor corrector method
+    :param func: given function from the task
+    :param x_0: initial argument
+    :param y_0: initial value
+    :param segment_end: end of the interpolation segment
+    :param step: step for x nodes
+    :param accuracy: accuracy of the method
+    :return: x and y interpolation nodes
     """
-    runge_kutta_interpolation_segment_end = x_0 + step * accuracy
-    x_nodes, y_nodes = runge_kutta_interpolation(func, x_0, y_0,
-                                                 runge_kutta_interpolation_segment_end, step)
-
+    x_nodes = [x_0, ]
+    y_nodes = [y_0, ]
     while x_nodes[-1] <= segment_end:
-        func_temp = interpolate.lagrange(x_nodes[-accuracy:], y_nodes[-accuracy:])
-        y_temp = y_nodes[-1] + integrate.quad(func_temp, x_nodes[-1], x_nodes[-1] + step)[0]
-
-        func_temp = interpolate.lagrange(x_nodes[-accuracy:], np.append(y_nodes[-accuracy: -1], [y_temp, ]))
-        y_node = y_nodes[-1] + integrate.quad(func_temp, x_nodes[-1], x_nodes[-1] + step)[0]
-
-        x_nodes = np.append(x_nodes, [x_nodes[-1] + step])
-        y_nodes = np.append(y_nodes, [y_node, ])
+        x_previous, y_previous = x_nodes[-1], y_nodes[-1]
+        y_node = y_nodes[-1] + step * func(x_previous, y_previous)
+        for attempt in range(1, accuracy + 1):
+            y_node = y_previous + (step / 2) * (func(x_previous, y_previous) + func(x_previous + step, y_node))
+        x_nodes.append(x_nodes[-1] + step)
+        y_nodes.append(y_node)
 
     return x_nodes, y_nodes
 
@@ -65,6 +59,11 @@ def adam_prognosis_correction_method(func, x_0, y_0, segment_end, step, accuracy
 def given_function(x, y):
     """ Given function from the task """
     return x**2 + 2 * x + y / (x + 2)
+
+
+def explicit_solution(x):
+    """ Explicit solution of the given task """
+    return 0.5 * (x**3 + 2 * x**2 + 1.51 * x + 3.02)
 
 
 def main():
@@ -75,12 +74,18 @@ def main():
 
     x_nodes_list, y_nodes_list = [], []
     for accuracy in range(2, 5):
-        x_nodes, y_nodes = adam_prognosis_correction_method(given_function, x_0, y_0, segment_end, step, accuracy)
+        x_nodes, y_nodes = predictor_corrector_method(given_function, x_0, y_0, segment_end, step, accuracy)
 
         x_nodes_list.append(x_nodes)
         y_nodes_list.append(y_nodes)
 
     display_plot_async(x_nodes_list, y_nodes_list)
+
+    print('Predictor-corrector interpolation results')
+    print(tp.header(['X', 'Y', 'Y2', 'Y3', 'Y4']))
+    for x, y, y2, y3, y4 in zip(x_nodes_list[0], [explicit_solution(x) for x in x_nodes_list[0]],
+                                y_nodes_list[0], y_nodes_list[1], y_nodes_list[2]):
+        print(tp.row([x, y, y2, y3, y4, ]))
 
 
 if __name__ == '__main__':
