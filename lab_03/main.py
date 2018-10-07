a0a2987eb2b1e9d0b1d0690161ca96d606e343d5
Igor Zaharov <lars.zaharov@gmail.com>
2018-10-07 12:16:55 +0300

    Add adams method to the 3-d lab

--- lab_03/main.py
+++ lab_03/main.py
@@ -6,9 +6,11 @@
     Adams prognosis-correction method of 2, 3, 4-th orders
 """
 
+import numpy as np
 import tableprint as tp
 import matplotlib.pyplot as plt
 from multiprocessing import Process
+from scipy import interpolate, integrate
 
 
 def _display_plot(x_nodes_list, y_nodes_list):
@@ -32,7 +34,36 @@ def display_plot_async(x_nodes_list, y_nodes_list):
     process.start()
 
 
-def predictor_corrector_method(func, x_0, y_0, segment_end, step, accuracy):
+def adams_prognosis_correction_method(func, x_0, y_0, segment_end, step, accuracy):
+    """
+        Adams predictor corrector method
+    :param func: given function from the task
+    :param x_0: initial argument
+    :param y_0: initial value
+    :param segment_end: end of the interpolation segment
+    :param step: step for x nodes
+    :param accuracy: accuracy of the method
+    :return: x and y interpolation nodes
+    """
+    initial_segment_end = x_0 + step * accuracy
+    x_nodes, y_nodes = euler_predictor_corrector_method(func, x_0, y_0, initial_segment_end, step, accuracy=4)
+
+    while x_nodes[-1] <= segment_end:
+        lagrange_pol = interpolate.lagrange(x_nodes[-accuracy:], y_nodes[-accuracy:])
+        lagrange_pol_integral = integrate.quad(lagrange_pol, x_nodes[-1], x_nodes[-1] + step)
+        y_temp = y_nodes[-1] + lagrange_pol_integral[0]
+
+        lagrange_pol = interpolate.lagrange(x_nodes[-accuracy:], np.append(y_nodes[-accuracy: -1], [y_temp, ]))
+        lagrange_pol_integral = integrate.quad(lagrange_pol, x_nodes[-1], x_nodes[-1] + step)
+        y_node = y_nodes[-1] + lagrange_pol_integral[0]
+
+        x_nodes = np.append(x_nodes, [x_nodes[-1] + step, ])
+        y_nodes = np.append(y_nodes, [y_node, ])
+
+    return x_nodes, y_nodes
+
+
+def euler_predictor_corrector_method(func, x_0, y_0, segment_end, step, accuracy):
     """
         Explicit Euler predictor corrector method
     :param func: given function from the task
@@ -74,14 +105,14 @@ def main():
 
     x_nodes_list, y_nodes_list = [], []
     for accuracy in range(2, 5):
-        x_nodes, y_nodes = predictor_corrector_method(given_function, x_0, y_0, segment_end, step, accuracy)
+        x_nodes, y_nodes = adams_prognosis_correction_method(given_function, x_0, y_0, segment_end, step, accuracy)
 
         x_nodes_list.append(x_nodes)
         y_nodes_list.append(y_nodes)
 
     display_plot_async(x_nodes_list, y_nodes_list)
 
-    print('Predictor-corrector interpolation results')
+    print('Adams predictor-corrector interpolation results')
     print(tp.header(['X', 'Y', 'Y2', 'Y3', 'Y4']))
     for x, y, y2, y3, y4 in zip(x_nodes_list[0], [explicit_solution(x) for x in x_nodes_list[0]],
                                 y_nodes_list[0], y_nodes_list[1], y_nodes_list[2]):
